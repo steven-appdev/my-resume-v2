7acb575c81b53a4c83efc7ee4d377ae177de74a8
steven-appdev <steventan@outlook.my>
2026-01-24 15:56:40 +0800

    Fix footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <div className="flex flex-col h-40 items-center border-t pt-4 border-white/20">
       <p className="text-white">
-        &copy; 2026 Steven's Portfolio. All rights reserved.
+        &copy;2026 Steven&apos;s Portfolio. All rights reserved.
       </p>
     </div>
   );
